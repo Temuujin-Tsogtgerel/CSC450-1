e5aba698d60f929b944eaf99702a347140c779ef
Temuujin-Tsogtgerel <asutemka@gmail.com>
2025-01-30 11:46:52 -0600

    Week7

---
+++ src/Week1/CSC450_CT1_mod1-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+
+// Standard namespace declaration
+using namespace std;
+
+// Main Function
+int main()
+{
+     double myMoney = 1000.50; // This should be printed out
+
+     // Standard Output Statements
+     cout << "Please be sure to correct all syntax errors in this program" << endl;
+     cout << "I have corrected all errors for this program." << endl;
+     cout << "The total amount of money available is = " << myMoney << endl;
+
+     // Wait For Output Screen
+     getch(); // Waits for a key press
+
+     // Main Function return Statement
+     return 0;
+}
